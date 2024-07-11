b42176ca133eb1dec37692cf1fe395a1783f4539
devCaiqueDePaula <dev.caiquedepaula@gmail.com>
2024-07-10 22:19:03 -0300

    Initial commit, project structuring

---
+++ ProductsApiRestfull/src/main/java/com/java/ProductsApiRestfull/ProductsApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.java.ProductsApiRestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiRestfulApplication.class, args);
+	}
+
+}
---
+++ ProductsApiRestfull/src/test/java/com/java/ProductsApiRestfull/ProductsApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ProductsApiRestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
